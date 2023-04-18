ebf810d31d008337aae188b25fe0595cd06fbe01
kipchumba-ket <kipchumbaelly97@gmail.com>
2023-04-18 17:37:50 +0300

    added application controller

--- protracker-back/app/controllers/application_controller.rb
+++ protracker-back/app/controllers/application_controller.rb
@@ -1,2 +1,87 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+
+    include ActionController::Cookies
+
+    include Pundit::Authorization
+
+    before_action :current_user
+
+    before_action :update_last_seen_at, if: -> { !current_user.nil? && (current_user.last_seen_at.nil? || current_user.last_seen_at <  2.minutes.ago) }
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+    def update_last_seen_at
+        current_user.update_attribute(:last_seen_at, Time.current)
+    end
+
+    def online_users
+        users = User.all
+        online_users = users.where("last_seen_at < ?", 5.minutes.ago)
+    end
+
+     # hash data into web token
+     def encode(uid, email)
+        payload = {
+            data: {
+                uid: uid,
+                email: email,
+                role: 'admin'
+            },
+        }
+        JWT.encode(payload, 'task_manager' , 'HS256')
+    end
+
+    # unhash the token
+    def decode(token)
+        JWT.decode(token, 'task_manager' , true, { algorithm: 'HS256' })
+    end
+
+    # verify authorization headers
+    def verify_auth
+        auth_headers = request.headers['Authorization']
+        if !auth_headers
+            render json: auth_headers
+        else
+            token = auth_headers.split(' ')[1]
+            save_user_id(token)
+        end
+    end
+
+    # save user's id
+    def save_user_id(token)
+        @uid = decode(token)[0]["data"]["uid"].to_i
+        save_user(@uid)
+    end
+
+    def save_user(uid)
+        session[:user_id] = uid
+    end
+
+    # delete user id in session
+    def remove_user
+        session.delete(:user_id)
+        render json: { message: "Logged out successfully"}
+    end
+    
+    # get logged in user
+    def current_user
+        user_now ||= User.find_by(id: session[:user_id]) 
+    end
+
+      
+        private
+      
+    def authenticate_user!
+          if !current_user
+            render json: { error: "You aren't signed in or signed up" }, status: :unauthorized
+          end
+    end
+      
+    # def admin
+    #        current_user.admin?
+    # end
+    
+    def user_not_authorized
+        render json: { message: "You are not authorized to perform this action." }, status: :unauthorized
+    end
+
 end
