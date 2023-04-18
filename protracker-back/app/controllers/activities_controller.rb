78d169cc138e6311570e22ad73942764cbea7f67
Jeff <jeffgichuki68@gmail.com>
2023-04-18 20:32:07 +0300

    added

--- protracker-back/app/controllers/activities_controller.rb
+++ protracker-back/app/controllers/activities_controller.rb
@@ -1,2 +1,8 @@
 class ActivitiesController < ApplicationController
-end
+    def get_activities
+      activities = PublicActivity::Activity.all.where("parameters != '{}'").map do |activity|
+        "#{activity.parameters[:user]} #{activity.parameters[:task]} #{activity.parameters[:created_at]}"
+      end
+      render json: activities
+    end
+  end
