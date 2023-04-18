5c0b4edd2d50398c5bb789844ee5b3974c4f153f
kipchumba-ket <kipchumbaelly97@gmail.com>
2023-04-18 17:39:54 +0300

    added sudent cohort controller

--- protracker-back/app/controllers/cohorts_controller.rb
+++ protracker-back/app/controllers/cohorts_controller.rb
@@ -1,2 +1,84 @@
 class CohortsController < ApplicationController
+
+    # create a new cohort;
+    # * cleared
+    def create
+        new_cohort = current_user.owned_cohorts.build(cohort_params)
+
+        authorize new_cohort
+        
+        if !new_cohort.save
+            render json: { errors: new_cohort.errors.full_messages }, status: :unprocessable_entity
+            return
+        end
+
+        new_cohort.cohort_students.create(user: current_user)
+
+        render json: { message: 'Cohort created successfully'}, status: :created
+
+    end
+
+    
+    # updating a cohort
+    #  * cleared
+    def update
+        updated_cohort = current_user.owned_cohorts.find_by(id: params[:cohort_id])
+        if updated_cohort.update(cohort_params)
+          render json: {message: "Cohort updated successfully", data: updated_cohort}, status: :ok
+        else
+          render json: { errors: updated_cohort.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+
+    #retrieve all cohort members;
+    # * cleared
+    def all_members
+        members = Cohort.find_by(id: params[:cohort_id].to_i).users
+        render json:  members, include: :projects, status: :ok
+    end
+
+
+
+    # retrieve cohort admin;
+    # * cleared
+    def cohort_admin
+        current_cohort = Cohort.find_by(id: params[:cohort_id].to_i)
+        if current_cohort
+            cohort_admin = User.find_by(id: current_cohort.user_id)
+            render json: { admin: cohort_admin }, status: :ok
+        else
+            render json: { message: 'Cohort does not exist' }, status: :not_found
+        end
+    end
+
+
+    # retrieving all corhots a user is part of;
+    # * cleared
+    def my_cohorts
+        cohorts = current_user.enrolled_cohorts
+        render json: cohorts, status: :ok
+    end
+
+
+    # retrieving all corhots a user is an admin;
+    # * cleared
+    def admin_cohorts           
+        cohorts = current_user.owned_cohorts
+        render json: cohorts, include: :users
+    end
+
+
+
+    private
+
+    def cohort_params
+        params.permit(:name, :start_date, :end_date)
+    end
+
+    def selected_cohort(cid)
+       cohort = current_user.owned_cohorts.find_by(cid)
+    end
+
+
 end
