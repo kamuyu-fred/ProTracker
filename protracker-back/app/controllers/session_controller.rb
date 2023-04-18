5aa76eed2108e58dc3cc9c863eea15b3d8b1b46f
Jeff <jeffgichuki68@gmail.com>
2023-04-18 19:53:29 +0300

    added user login and logout logic

--- protracker-back/app/controllers/session_controller.rb
+++ protracker-back/app/controllers/session_controller.rb
@@ -1,2 +1,28 @@
 class SessionController < ApplicationController
+
+    def login
+        user = User.find_by(email: params[:email])
+        if user&.authenticate(params[:password])
+          token = encode(user.id, user.email)
+          save_user_id(token)
+          render json: { message: "Logged in successfully", user: user ,  token: token}, status: :ok
+        else
+          render json: { error: "Invalid email or password" }, status: :unauthorized
+        end
+      end
+  
+  
+      def check_out
+        if current_user
+            render json: current_user
+        else
+          render json: { message: "No user is logged in", data: @current_user}
+        end
+      end
+  
+  
+      def destroy 
+         remove_user
+      end
+
 end
