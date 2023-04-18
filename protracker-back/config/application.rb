51c0c96362dc440a1ccd5fef4d06ce18bbb391d0
Jeff <jeffgichuki68@gmail.com>
2023-04-18 19:49:21 +0300

    added cookies configuration

--- protracker-back/config/application.rb
+++ protracker-back/config/application.rb
@@ -13,12 +13,9 @@ module ProtrackerBack
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.action_dispatch.cookies_same_site_protection = :strict
+
   end
 end
