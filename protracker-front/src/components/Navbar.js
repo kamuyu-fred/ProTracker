366b953a5322fc6583bb15435a0a58546c118016
fred <daktarispacetime@gmail.com>
2023-04-19 07:52:22 +0300

    cleaned up navbar dropdown

--- protracker-front/src/components/Navbar.js
+++ protracker-front/src/components/Navbar.js
@@ -22,8 +22,8 @@ function Navbar() {
 
             <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
                 <div class="px-4 py-3">
-                <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-                <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                <span class="block text-sm text-gray-900 dark:text-white">Jeff Maina</span>
+                <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">jeff.maina@student.moringaschool.com</span>
                 </div>
                 <ul class="py-2" aria-labelledby="user-menu-button">
                 <li>
@@ -32,10 +32,7 @@ function Navbar() {
                 <li>
                     <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
                 </li>
-                <li>
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
-                </li>
-                <li>
+               <li>
                     <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
                 </li>
                 </ul>
