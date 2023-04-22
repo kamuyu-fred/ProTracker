e643cb0eab73d74184b8939a8679b26b382cc2ef
fred <daktarispacetime@gmail.com>
2023-04-23 00:43:50 +0300

    fixed notification avatar visibility

--- protracker-front/src/components/Navbar.js
+++ protracker-front/src/components/Navbar.js
@@ -36,7 +36,7 @@ function Navbar() {
               <div class="divide-y divide-gray-100 dark:divide-gray-700">
                 <a href="#" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
                   <div class="flex-shrink-0">
-                    <img class="rounded-full w-11 h-11" src="https://flowbite.com//docs/images/people/profile-picture-1.jpg" alt="Jese image"/>
+                    <img class="rounded-full w-11 h-11" src="https://flowbite.com/docs/images/people/profile-picture-1.jpg" alt="Jese image"/>
                     <div class="absolute flex items-center justify-center w-5 h-5 ml-6 -mt-5 bg-blue-600 border border-white rounded-full dark:border-gray-800">
                       <svg class="w-3 h-3 text-white" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
                     </div>
@@ -48,7 +48,7 @@ function Navbar() {
                 </a>
                 <a href="#" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
                   <div class="flex-shrink-0">
-                    <img class="rounded-full w-11 h-11" src="https://flowbite.com//docs/images/people/profile-picture-2.jpg" alt="Joseph image"/>
+                    <img class="rounded-full w-11 h-11" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Joseph image"/>
                     <div class="absolute flex items-center justify-center w-5 h-5 ml-6 -mt-5 bg-gray-900 border border-white rounded-full dark:border-gray-800">
                       <svg class="w-3 h-3 text-white" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"></path></svg>
                     </div>
@@ -60,7 +60,7 @@ function Navbar() {
                 </a>
                 <a href="#" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
                   <div class="flex-shrink-0">
-                    <img class="rounded-full w-11 h-11" src="https://flowbite.com//docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+                    <img class="rounded-full w-11 h-11" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
                     <div class="absolute flex items-center justify-center w-5 h-5 ml-6 -mt-5 bg-red-600 border border-white rounded-full dark:border-gray-800">
                       <svg class="w-3 h-3 text-white" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd"></path></svg>
                     </div>
@@ -72,7 +72,7 @@ function Navbar() {
                 </a>
                 <a href="#" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
                   <div class="flex-shrink-0">
-                    <img class="rounded-full w-11 h-11" src="https://flowbite.com//docs/images/people/profile-picture-4.jpg" alt="Leslie image"/>
+                    <img class="rounded-full w-11 h-11" src="https://flowbite.com/docs/images/people/profile-picture-4.jpg" alt="Leslie image"/>
                     <div class="absolute flex items-center justify-center w-5 h-5 ml-6 -mt-5 bg-green-400 border border-white rounded-full dark:border-gray-800">
                       <svg class="w-3 h-3 text-white" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                     </div>
@@ -84,7 +84,7 @@ function Navbar() {
                 </a>
                 <a href="#" class="flex px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700">
                   <div class="flex-shrink-0">
-                    <img class="rounded-full w-11 h-11" src="https://flowbite.com//docs/images/people/profile-picture-5.jpg" alt="Robert image"/>
+                    <img class="rounded-full w-11 h-11" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="Robert image"/>
                     <div class="absolute flex items-center justify-center w-5 h-5 ml-6 -mt-5 bg-purple-500 border border-white rounded-full dark:border-gray-800">
                       <svg class="w-3 h-3 text-white" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
                     </div>
