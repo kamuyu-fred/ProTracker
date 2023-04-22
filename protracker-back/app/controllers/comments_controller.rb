6a9a10bcd91f90c39fb626d1bc50c1917ef0e959
Jeff <jeffgichuki68@gmail.com>
2023-04-22 15:11:10 +0300

    merged frontend testing with fred branch2

--- protracker-back/app/controllers/comments_controller.rb
+++ protracker-back/app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
     # * cleared
     def post_comment
         project = Project.find(params[:project_id])
+        current_user = User.find(params[:user_id])
         comment = project.comments.build(comment_params)
         comment.user = current_user
     
@@ -25,7 +26,7 @@ class CommentsController < ApplicationController
 
             render json: { message: "Comment saved successfully" }, status: :created
         else
-           render json: { message: "Couldn't create comment" }, status: :unprocessable_entity
+           render json: { message: "Couldn't create comment", errors: comment.errors.full_messages }, status: :unprocessable_entity
         end
     end
 
@@ -35,6 +36,7 @@ class CommentsController < ApplicationController
 
         parent_comment = Comment.find_by(id: params[:comment_id])
         project = parent_comment.project
+        current_user = User.find_by(id: params[:user_id])
         reply = parent_comment.replies.build(comment_params)
         reply.user = current_user
         reply.project = project
