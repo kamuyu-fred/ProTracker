490aceabf276ce98cb089891afb5523839a16afc
fred <daktarispacetime@gmail.com>
2023-04-22 20:05:38 +0300

    modded sidebar

--- protracker-front/src/components/Sidebar.js
+++ protracker-front/src/components/Sidebar.js
@@ -45,9 +45,7 @@ function Sidebar() {
                   <li>
                      <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">User Directory</a>
                   </li>
-                  <li>
-                     <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Invoice</a>
-                  </li>
+                  
             </ul>
          </li>
          <li>
