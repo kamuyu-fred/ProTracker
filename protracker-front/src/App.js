ad765f3351eeeff1f139c5769990e854606b8878
abel.rutto@student.moringaschool.com <abel.rutto@student.moringaschool.com>
2023-04-24 10:11:51 +0300

    added signup

--- protracker-front/src/App.js
+++ protracker-front/src/App.js
@@ -1,23 +1,19 @@
-import { Route, RouterProvider, Routes } from "react-router-dom";
 import './App.css';
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-
+import rocketImg from './Asserts/rocket.png';
+import { Signup } from './components/Signup';
 
 function App() {
   return (
-  <>
-
-    <Navbar />
-    <Sidebar />
-  
-    <div className="App">
-
-
-      
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-5">
+          <Signup />
+        </div>
+        <div className="col-md-7 my-auto">
+          <img className="img-fluid w-100" src={rocketImg} alt=""/>
+        </div>
+      </div>
     </div>
-
-</>
   );
 }
 
