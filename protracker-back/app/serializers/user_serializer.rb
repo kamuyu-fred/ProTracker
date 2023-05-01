37ec8fd0d883ec50a2cf7011c8488c0d28304af4
Jeff <jeffgichuki68@gmail.com>
2023-05-01 23:59:41 +0300

    added

--- protracker-back/app/serializers/user_serializer.rb
+++ protracker-back/app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :admin, :bio, :github_link, :achievements, :avatar_url, :online_status, :enrolled_cohorts
+  attributes :id, :username, :email, :admin, :bio, :github_link, :achievements, :avatar_url, :online_status, :enrolled_cohorts, :enrolled_projects
   has_many :achievements
   has_many :enrolled_projects
   has_many :projects
