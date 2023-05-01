0953672f398b2ea2537e4a9572a184007b432256
Jeff <jeffgichuki68@gmail.com>
2023-05-01 16:06:10 +0300

    added

--- protracker-back/bin/render-build.sh
+++ protracker-back/bin/render-build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+bundle install
+
+# bundle exec rails db:drop DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
