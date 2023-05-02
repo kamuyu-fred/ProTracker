8eda84647758cb3079ad03d9bbc010994fb35096
fred <daktarispacetime@gmail.com>
2023-05-02 08:45:28 +0300

    fixed navbar

--- protracker-front/src/components/navBar/Navbar.js
+++ protracker-front/src/components/navBar/Navbar.js
@@ -230,8 +230,8 @@ function Navbar() {
   return (
     <>
       <nav class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" class="flex items-center">
+        <div class="max-w-screen-xl flex justify-items-center mx-4 p-2">
+          <a href="https://flowbite.com/" class="flex items-start">
           <img
               src={logo}
               class="h-12 mr-1"
