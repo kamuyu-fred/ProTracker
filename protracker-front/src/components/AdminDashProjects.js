ce06fcf6d1a7ece288c4c713b1d7e31da1a7a469
fred <daktarispacetime@gmail.com>
2023-05-03 21:08:40 +0300

    Added link to backend documentation and cleaned up Readmes

--- protracker-front/src/components/AdminDashProjects.js
+++ protracker-front/src/components/AdminDashProjects.js
@@ -105,7 +105,7 @@ function AdminDashProjects() {
 
   return (
     <div>
-      <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
+      <section class="bg-white dark:bg-gray-900 p-3 sm:p-5">
      {isFetching ?  ( <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
           <div style={{width: "100%"}} class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
             <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
